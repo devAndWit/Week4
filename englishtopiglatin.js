03e1613442ca01ebe8138bd98e2a198bf75c7e63
Andre Witt <dev.code@h-town.net>
2024-11-27 23:36:05 +0100

    ADD: create class piglatin and add methods

--- englishtopiglatin.js
+++ englishtopiglatin.js
@@ -26,3 +26,105 @@
 //
 // rule 3 - start with vowel => word + way
 // Example: Awesome = Awesome +way = Awesomeway
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+class PigLatin {
+  normalText = "";
+  pigText = "";
+  arrWords = [];
+
+  constructor() {}
+
+  clearAllValues() {
+    this.normalText = "";
+    this.pigText = "";
+    this.arrWords = [];
+  }
+
+  setText(text) {
+    this.normalText = text.trim();
+    this.setArrWords(this.normalText);
+    this.translate();
+  }
+
+  setArrWords(text) {
+    this.arrWords = this.normalText.split(" ");
+  }
+
+  translate() {
+    this.arrWords.forEach((word) => {
+        
+    });
+  }
+
+  getTemp() {
+    console.log(this.normalText);
+    console.log(this.arrWords.join(" "));
+  }
+
+  getOutputText(type) {
+    let output = "";
+
+    switch (type) {
+      case 1:
+        output = "\n";
+        output +=
+          "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+        output += "\n";
+        output += "   Please type your message";
+        output += "\n";
+        output +=
+          "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+        output += "\n";
+        output += "   >  ";
+        break;
+
+      case 2:
+        output = "\n";
+        output +=
+          "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+        output += "\n";
+        output += "   Your message:" + this.normalText;
+        output += "\n";
+        output += "   Pig Latin:" + this.pigText;
+        output += "\n";
+        output +=
+          "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+        output += "\n";
+        break;
+    }
+
+    return output;
+  }
+}
+
+let obj = new PigLatin();
+
+obj.setText("Pig Latin is hard to speak");
+obj.getTemp();
+
+// process.stdout.write(obj.getOutputText(1));
+
+// process.stdin.setEncoding("utf8");
+
+// let state = 1;
+
+// process.stdin.on("data", (input) => {
+//   input = input.toString().trim();
+
+//   if (state === 1) {
+//     if (input.length > 0) {
+//       obj.setText(input);
+//       state = 2;
+//     } else {
+//       console.clear();
+//       process.stdout.write(obj.getOutputText(1));
+//     }
+//   }
+
+//   if (state === 2) {
+//     console.clear();
+//     process.stdout.write(obj.getOutputText(2));
+//     process.exit(null);
+//   }
+// });
